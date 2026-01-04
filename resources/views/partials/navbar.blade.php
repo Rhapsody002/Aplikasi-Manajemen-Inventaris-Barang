fbe727c290448a19640667b5d52c3c808921e9a7
Rhapsody002 <dzaky24ti@mahasiswa.pcr.ac.id>
2026-01-04 10:34:30 +0700

    navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,72 +1,87 @@
-<header class="navbar pcoded-header navbar-expand-lg headerpos-fixed">
-    <div class="m-header">
-        <a class="mobile-menu" id="mobile-collapse" href="#">
-            <span></span>
-        </a>
+<header class="app-header">
+
+    {{-- LEFT --}}
+    <div class="header-left">
+        <h5 class="page-title">
+            @yield('title', 'Dashboard')
+        </h5>
     </div>
 
-    <div class="collapse navbar-collapse">
-        <ul class="navbar-nav ml-auto align-items-center">
+    {{-- RIGHT --}}
+    <div class="header-right">
 
-            {{-- NOTIF TUGAS --}}
-            @if($notifTasks > 0)
-            <li class="nav-item">
-                <a class="nav-link position-relative" href="{{ route('tasks.my') }}">
-                    <i class="feather icon-bell"></i>
-                    <span class="badge badge-danger badge-dot">
-                        {{ $notifTasks }}
-                    </span>
-                </a>
-            </li>
-            @endif
+        {{-- 🔔 NOTIFICATION --}}
+        <div class="dropdown">
+            <a href="#" class="header-icon" data-toggle="dropdown">
+                <i class="feather icon-bell"></i>
 
-            {{-- STOK KRITIS --}}
-            @if($stokKritis > 0)
-            <li class="nav-item">
-                <a class="nav-link position-relative" href="{{ route('barang.index') }}">
-                    <i class="feather icon-alert-circle text-danger"></i>
-                    <span class="badge badge-danger badge-dot">
-                        {{ $stokKritis }}
-                    </span>
-                </a>
-            </li>
-            @endif
+                @if(($notifTasks ?? 0) > 0 || ($stokKritis ?? 0) > 0)
+                    <span class="badge-dot bg-danger"></span>
+                @endif
+            </a>
 
-            {{-- USER --}}
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle d-flex align-items-center"
-                    data-toggle="dropdown"
-                    href="#">
+            <div class="dropdown-menu dropdown-menu-right notification-dropdown">
 
-                    <img src="{{ auth()->user()->foto_profil
-                        ? asset('storage/'.auth()->user()->foto_profil)
-                        : asset('assets/images/user/default.png') }}"
-                        class="rounded-circle mr-2"
-                        width="36"
-                        height="36"
-                        style="object-fit:cover">
+                <h6 class="dropdown-header">Notifikasi</h6>
 
-                    <span class="fw-semibold">
-                        {{ auth()->user()->name }}
-                    </span>
-                </a>
-
-                <div class="dropdown-menu dropdown-menu-right">
-                    <a class="dropdown-item" href="{{ route('profile.index') }}">
-                        <i class="feather icon-user mr-2"></i> Profile
+                {{-- TUGAS --}}
+                @if(($notifTasks ?? 0) > 0)
+                    <a class="dropdown-item d-flex align-items-center"
+                       href="{{ auth()->user()->role === 'petugas'
+                                ? route('tasks.my')
+                                : route('tasks.index') }}">
+                        <i class="feather icon-clipboard text-warning mr-2"></i>
+                        <div>
+                            <strong>{{ $notifTasks }}</strong> tugas pending
+                        </div>
                     </a>
+                @endif
 
-                    <div class="dropdown-divider"></div>
+                {{-- STOK KRITIS --}}
+                @if(($stokKritis ?? 0) > 0)
+                    <a class="dropdown-item d-flex align-items-center"
+                       href="{{ route('barang.index') }}">
+                        <i class="feather icon-alert-circle text-danger mr-2"></i>
+                        <div>
+                            <strong>{{ $stokKritis }}</strong> stok kritis
+                        </div>
+                    </a>
+                @endif
 
-                    <form action="{{ route('logout') }}" method="POST">
-                        @csrf
-                        <button class="dropdown-item text-danger">
-                            <i class="feather icon-log-out mr-2"></i> Logout
-                        </button>
-                    </form>
-                </div>
-            </li>
+                {{-- EMPTY --}}
+                @if(($notifTasks ?? 0) === 0 && ($stokKritis ?? 0) === 0)
+                    <div class="dropdown-item text-muted text-center small">
+                        Tidak ada notifikasi
+                    </div>
+                @endif
 
-        </ul>
+            </div>
+        </div>
+
+        {{-- 👤 USER --}}
+        <div class="dropdown">
+            <a href="#" class="user-trigger" data-toggle="dropdown">
+                <img src="{{ auth()->user()->foto_profil
+                    ? asset('storage/'.auth()->user()->foto_profil)
+                    : asset('assets/images/user/default.png') }}">
+                <span>{{ auth()->user()->name }}</span>
+            </a>
+
+            <div class="dropdown-menu dropdown-menu-right">
+                <a href="{{ route('profile.index') }}" class="dropdown-item">
+                    <i class="feather icon-user"></i> Profile
+                </a>
+
+                <div class="dropdown-divider"></div>
+
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button class="dropdown-item text-danger">
+                        <i class="feather icon-log-out"></i> Logout
+                    </button>
+                </form>
+            </div>
+        </div>
+
     </div>
 </header>
