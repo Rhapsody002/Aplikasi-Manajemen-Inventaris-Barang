0970dddd24b94d84a4a23010e3d569b8e975f268
Rhapsody002 <dzaky24ti@mahasiswa.pcr.ac.id>
2026-01-04 11:01:54 +0700

    still navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,49 +1,51 @@
 <header class="navbar pcoded-header navbar-expand-lg navbar-light headerpos-fixed">
 
     {{-- LEFT --}}
-    <div class="m-header d-flex align-items-center gap-2">
+    <div class="m-header d-flex align-items-center gap-3">
         <a class="mobile-menu" id="mobile-collapse" href="#!">
             <span></span>
         </a>
 
-        <span class="fw-bold text-dark ms-2">
-            @yield('title', 'Dashboard')
-        </span>
+        {{-- PAGE TITLE --}}
+        <div class="header-title">
+            <h6 class="mb-0 fw-bold">
+                @yield('title', 'Dashboard')
+            </h6>
+            <small class="text-muted">
+                Ringkasan aktivitas gudang
+            </small>
+        </div>
     </div>
 
     {{-- RIGHT --}}
-    @auth
     <div class="collapse navbar-collapse">
         <ul class="navbar-nav ms-auto align-items-center gap-3">
 
             {{-- NOTIFICATION --}}
             <li class="nav-item dropdown">
-                <a class="nav-link"
-                    href="#"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false">
-                    <i class="feather icon-bell"></i>
+                <a class="nav-link position-relative" href="#" data-bs-toggle="dropdown">
+                    <i class="feather icon-bell fs-5"></i>
 
                     @if(($notifTasks ?? 0) + ($stokKritis ?? 0) > 0)
-                    <span class="badge badge-danger badge-dot">
+                    <span class="badge-dot">
                         {{ ($notifTasks ?? 0) + ($stokKritis ?? 0) }}
                     </span>
                     @endif
                 </a>
 
-                <div class="dropdown-menu dropdown-menu-end">
+                <div class="dropdown-menu dropdown-menu-end notification-dropdown">
                     <h6 class="dropdown-header">Notifikasi</h6>
 
                     @if(($notifTasks ?? 0) > 0)
                     <a class="dropdown-item" href="{{ route('tasks.my') }}">
-                        <i class="feather icon-clipboard me-2"></i>
+                        <i class="feather icon-clipboard text-warning me-2"></i>
                         {{ $notifTasks }} tugas pending
                     </a>
                     @endif
 
                     @if(($stokKritis ?? 0) > 0)
                     <a class="dropdown-item" href="{{ route('barang.index') }}">
-                        <i class="feather icon-alert-circle me-2"></i>
+                        <i class="feather icon-alert-circle text-danger me-2"></i>
                         {{ $stokKritis }} stok kritis
                     </a>
                     @endif
@@ -58,9 +60,7 @@
 
             {{-- USER --}}
             <li class="nav-item dropdown">
-                <a class="nav-link d-flex align-items-center gap-2"
-                    href="#"
-                    data-bs-toggle="dropdown">
+                <a class="nav-link d-flex align-items-center gap-2" href="#" data-bs-toggle="dropdown">
                     <img src="{{ auth()->user()->foto_profil
                         ? asset('storage/'.auth()->user()->foto_profil)
                         : asset('assets/images/user/default.png') }}"
@@ -68,7 +68,10 @@
                         width="36"
                         height="36"
                         style="object-fit:cover">
-                    <span class="fw-semibold">{{ auth()->user()->name }}</span>
+
+                    <span class="fw-semibold">
+                        {{ auth()->user()->name }}
+                    </span>
                 </a>
 
                 <div class="dropdown-menu dropdown-menu-end">
@@ -89,6 +92,5 @@
 
         </ul>
     </div>
-    @endauth
 
 </header>
